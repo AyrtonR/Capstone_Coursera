a1052a569f86c013d2d647692af4c0a2d358da6f
Ayrton Ramirez <ayrtonramirez2018@gmail.com>
2023-08-14 03:09:31 +0300

    Project

--- coursera_capstone/src/App.js
+++ coursera_capstone/src/App.js
@@ -1,23 +1,152 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [guests, setGuests] = useState(1);
+  const [occasion, setOccasion] = useState('none');
+  const [reservationStatus, setReservationStatus] = useState(null);
+
+  // Function to generate available time options between 17:00 and 23:00 in 30-minute intervals
+  const generateAvailableTimeOptions = () => {
+    const startTime = 17;
+    const endTime = 23;
+    const timeOptions = [];
+
+    for (let hour = startTime; hour <= endTime; hour++) {
+      for (let minute = 0; minute < 60; minute += 30) {
+        const timeValue = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
+        timeOptions.push(<option key={timeValue} value={timeValue}>{timeValue}</option>);
+      }
+    }
+
+    return timeOptions;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!time || occasion === 'none') {
+      setReservationStatus('required');
+    } else {
+      setTimeout(() => {
+        setReservationStatus('success');
+        // Reset form fields after successful booking
+        setName('');
+        setPhoneNumber('');
+        setDate('');
+        setTime('');
+        setGuests(1);
+        setOccasion('none');
+      }, 1000);
+    }
+  };
+
+  const handlePopupClose = () => {
+    setReservationStatus(null);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="top-bar">
+          <img src={process.env.PUBLIC_URL + '/logo.png'} alt="Little Lemon Restaurant" className="logo" />
+        </div>
+        <h1 className="header-title">Reserve a Table</h1>
       </header>
+      <section>
+        <form onSubmit={handleSubmit}>
+          <div className="form-section">
+            <div className="date-section">
+              {/* Date Input */}
+              <label htmlFor="date">Date:</label>
+              <input
+                type="date"
+                id="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                required
+              />
+            </div>
+            <div className="time-section">
+              {/* Time Select */}
+              <label htmlFor="time">Time:</label>
+              <select
+                id="time"
+                value={time}
+                onChange={(e) => setTime(e.target.value)}
+                required
+              >
+                <option value="">Select a time</option>
+                {generateAvailableTimeOptions()}
+              </select>
+            </div>
+           
+            <div className="guests-section">
+              {/* Guests Input */}
+              <label htmlFor="guests">Number of Guests:</label>
+              <input
+                type="number"
+                id="guests"
+                value={guests}
+                onChange={(e) => setGuests(e.target.value)}
+                min="1"
+                required
+              />
+            </div>
+            <div className="occasion-section">
+              {/* Occasion Select */}
+              <label htmlFor="occasion">Occasion:</label>
+              <select
+                id="occasion"
+                value={occasion}
+                onChange={(e) => setOccasion(e.target.value)}
+                required
+                >
+                <option value="none">Select an occasion</option>
+                <option value="birthday">Birthday</option>
+                <option value="meeting">Meeting</option>
+                <option value="anniversary">Anniversary</option>
+              </select>
+            </div>
+            <div className="name-section">
+              {/* Name Input */}
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="phone-section">
+              {/* Phone Number Input */}
+              <label htmlFor="phone">Phone Number:</label>
+              <input
+                type="tel"
+                id="phone"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                required
+              />
+            </div>
+            <div className="button-section">
+              <button type="submit" className="book-button">Book Table</button>
+            </div>
+          </div>
+        </form>
+        {reservationStatus === 'success' && (
+          <div className="success-popup">
+            <div className="success-message">
+              Booking successful! Your table has been reserved.
+            </div>
+            <button onClick={handlePopupClose} className="close-button">Close</button>
+          </div>
+        )}
+      </section>
     </div>
   );
 }
--- coursera_capstone/src/App.test.js
+++ coursera_capstone/src/App.test.js
@@ -1,8 +1,53 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; 
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the title', () => {
+  const { getByText } = render(<App />);
+  const titleElement = getByText(/Restaurant Table Booking/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test('renders the form with initial values', () => {
+  const { getByLabelText, getByDisplayValue } = render(<App />);
+  const dateInput = getByLabelText('Date:');
+  const timeInput = getByLabelText('Time:');
+  const guestsInput = getByLabelText('Number of Guests:');
+  const occasionSelect = getByLabelText('Occasion:');
+
+  expect(dateInput).toBeInTheDocument();
+  expect(timeInput).toBeInTheDocument();
+  expect(guestsInput).toBeInTheDocument();
+  expect(occasionSelect).toBeInTheDocument();
+
+  // Check initial values
+  expect(dateInput).toHaveValue('');
+  expect(timeInput).toHaveValue('');
+  expect(guestsInput).toHaveValue('1');
+  expect(occasionSelect).toHaveValue('none');
+});
+
+test('shows a "required" message when trying to submit without time and occasion', () => {
+  const { getByText, getByLabelText, getByDisplayValue } = render(<App />);
+  const submitButton = getByText('Book Table');
+  fireEvent.click(submitButton);
+
+  const errorMessage = getByText('Time and occasion are required.');
+  expect(errorMessage).toBeInTheDocument();
+});
+
+test('shows a "Reservation Successful" message when submitting with time and occasion', () => {
+  const { getByText, getByLabelText, getByDisplayValue } = render(<App />);
+  const timeInput = getByLabelText('Time:');
+  const occasionSelect = getByLabelText('Occasion:');
+  const submitButton = getByText('Book Table');
+
+  fireEvent.change(timeInput, { target: { value: '18:00' } });
+  fireEvent.change(occasionSelect, { target: { value: 'birthday' } });
+
+  fireEvent.click(submitButton);
+
+  const successMessage = getByText('Reservation Successful!');
+  expect(successMessage).toBeInTheDocument();
 });
